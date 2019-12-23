58047a2ac01ac515a10340c2d1689943a252dca5
spawn <y1-spawn@ik8s.com>
2019-12-23 21:11:00 +0800

    8545845

---
+++ template/src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+    <section>
+        <el-row>
+            <el-col :span="4">
+                <div style="background-color: rebeccapurple;width: 200px;height: 80px;"></div>
+            </el-col>
+            <el-col :span="16">
+                <div style="background-color: rebeccapurple;width: 200px;height: 80px;"></div>
+            </el-col>
+            <el-col :span="4">
+                <div style="background-color: rebeccapurple;width: 200px;height: 80px;"></div>
+            </el-col>
+        </el-row>
+    </section>
+</template>
+<script lang="ts">
+    import { Component,Vue } from 'vue-property-decorator'
+    @Component({})
+    export default class Header extends Vue{
+
+    }
+
+</script>
---
+++ template/src/views/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <section>
+        <Header/>
+        <router-view/>
+        <Footer/>
+    </section>
+</template>
+<script lang="ts">
+    import { Component, Vue } from "vue-property-decorator"
+    import  Header from '@/components/Header.vue'
+    import  Footer from '@/components/Footer.vue'
+    @Component({
+        components:{
+            Header,Footer
+        }
+    })
+    export default class About extends Vue{
+        
+    }
+</script>
